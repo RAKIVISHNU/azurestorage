9f45fa8ace94afe515a5a1ba10b84c23197e8d7b
vishnu <rakivishnu@gmail.com>
2024-11-08 16:01:48 -0500

    file updated

--- main.tf
+++ main.tf
@@ -1,35 +1,16 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">=4.2.0"
-    }
-    tls = {
-    source = "hashicorp/tls"
-    version = "~>4.0"
-      }
-  }
+variable azurerm_resource_group_name {
+type = string
 }
-
-provider "azurerm" {
-  subscription_id = "bc7add54-68b1-4720-a761-75ae4c82e200"
-  # client_id = "44e9c1cb-4aa4-4a1f-994d-e35f201193db"
-  # client_secret = "tcP8Q~ro5-.MSdnTgh1WPM4sYd_6chn~ZEywUcSa"
-  # tenant_id = "d77ec4e8-c88b-431b-8d7f-77755a4bbefc"
-  features {}
+variable azurerm_resource_group_location {
+type = string
 }
-
-resource "azurerm_resource_group" "rg" {
-  name = "vishnurgnew"
-  location = "east us"
+resource "azurerm_storage_account" "myStorage" {
+name = "vishnu03030"
+resource_group_name = var.azurerm_resource_group_name
+location = var.azurerm_resource_group_location
+account_tier = "Standard"
+account_replication_type = "GRS"
 }
-
-module "storage1" {
-source = "./storage"
-azurerm_resource_group_name = azurerm_resource_group.rg.name
-azurerm_resource_group_location = azurerm_resource_group.rg.location
-}
-
-output "storageaccount_replication" {
-  value = module.storage1.storage_replication
+output storage_replication {
+value = azurerm_storage_account.myStorage.account_replication_type
 }
